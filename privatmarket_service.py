a15a99b995338b2be40290e195eaf9809d22c9e6
Луговской Евгений Владимирович <evgenij.lugovskoj@privatbank.ua>
2018-03-12 16:16:06 +0200

    fixed create tender keyword

--- privatmarket_service.py
+++ privatmarket_service.py
@@ -12,7 +12,7 @@ import dateutil.parser
 def modify_test_data(initial_data):
     # set user name
     # initial_data['procuringEntity']['name'] = u'Товариство З Обмеженою Відповідальністю \'Мак Медіа Прінт\''
-    initial_data['procuringEntity']['name'] = u'Сф Рубіжне'
+    initial_data['procuringEntity']['name'] = u'ТОВ \'СФ \'РУБІЖНЕ\''
     if 'contactPoint' in initial_data['procuringEntity']:
         initial_data['procuringEntity']['contactPoint']['telephone'] = u'+380670444580'
         initial_data['procuringEntity']['contactPoint']['url'] = u'https://dadadad.com'
